6547da056974ea40a7affabd8f42b28ea6103edd
KelseyF624 <kelseyf.624@gmail.com>
2025-02-17 15:51:02 -0500

    Complete Commit

--- src/HighorLow.java
+++ src/HighorLow.java
@@ -1,8 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
 public class HighorLow {
     public static void main(String[] args) {
-
-
+        int guess;
+        Scanner input = new Scanner(System.in);
+        do {
+            Random rand = new Random();
+            int correctNum = rand.nextInt(10);
+            System.out.println("Pick a number from 1-10");
+            guess = input.nextInt();
+            if (guess < correctNum) {
+                System.out.println("The number you guessed is too low.");
+            } else if (guess > correctNum) {
+                System.out.println("The number you guessed is too high.");
+            } else if (guess == correctNum) {
+                System.out.println("The number you guessed is...RIGHT ON THE MONEY!");
+            }
+        }while (guess >= 0 || guess <=10 );
     }
 }
